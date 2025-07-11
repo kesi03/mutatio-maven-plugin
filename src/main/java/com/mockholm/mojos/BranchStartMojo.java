52fd06e25e713b1e309b2119fbd1ef4dfdcae341
kesi03 <68278724+kesi03@users.noreply.github.com>
2025-07-11 11:03:21 +0200

    added a common branch start and end

---
+++ src/main/java/com/mockholm/mojos/BranchStartMojo.java
@@ -0,0 +1,66 @@
+package com.mockholm.mojos;
+
+import com.mockholm.config.BranchType;
+import com.mockholm.models.MojoCommons;
+import com.mockholm.mojos.commons.BranchMojo;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Settings;
+
+/**
+ * This Mojo is used to start the branch.
+ * It is typically called at the beginning of the build process to initialize a branch.
+ * You must supply a type of branch and repoIndentity
+ */
+@Mojo(name = "branch-start", aggregator = true, defaultPhase = LifecyclePhase.NONE)
+public class BranchStartMojo extends AbstractMojo {
+
+    /**
+     * The Maven project being built.
+     * This is used to access project properties and configuration.
+     */
+    @Parameter( defaultValue = "${project}", readonly = true )
+    private MavenProject project;
+
+    /**
+     * The settings for the Maven build, which may include repository configurations.
+     * This is used to access settings defined in the Maven settings.xml file.
+     */
+    @Parameter( defaultValue = "${settings}", readonly = true )
+    private Settings settings;
+
+    /**
+     * The identity of the repository used to determine the branch to start.
+     * This is typically the name of the repository or a unique identifier.
+     */
+    @Parameter(property = "repoIdentity", name = "repoIdentity")
+    private String repoIdentity;
+
+    /**
+     * The Branch type {@link BranchType}
+     */
+    @Parameter(property = "branchType", name = "branchType")
+    private BranchType branchType;
+
+
+
+    /**
+     * Flag to determine whether to push changes to the remote repository after starting the archive branch.
+     * Default is true, meaning changes will be pushed.
+     */
+    @Parameter(property = "pushChanges", name ="pushChanges", defaultValue = "true")
+    private boolean pushChanges;
+
+    public void execute() {
+        new BranchMojo(new MojoCommons()
+                .withLog(getLog())
+                .withRepoIdentity(repoIdentity)
+                .withPushChanges(pushChanges)
+                .withProject(project)
+                .withSettings(settings))
+                .executeStart(branchType);
+    }
+}
